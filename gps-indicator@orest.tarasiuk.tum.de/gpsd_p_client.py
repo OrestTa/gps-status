5bcf84b789dc53657096651f6df3629c81c5df23
Orest Tarasiuk <orest.tarasiuk@tum.de>
2012-08-02 19:16:49 +0200

    update licensing info for the python client
    Signed-off-by: Orest Tarasiuk <orest.tarasiuk@tum.de>

--- gps-indicator@orest.tarasiuk.tum.de/gpsd_p_client.py
+++ gps-indicator@orest.tarasiuk.tum.de/gpsd_p_client.py
@@ -2,22 +2,23 @@
 #
 ############################################################################
 #
-# Copyright (C) 2012 Orest Tarasiuk <orest.tarasiuk@tum.de>
+# Copyright/Copyleft (C) 2012 Orest Tarasiuk <orest.tarasiuk@tum.de>
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 3 of the License, or (at your option) any later version.
+# This file is part of Gnome Shell Extension GPS Indicator (GSEGI).
 #
-# This library is distributed in the hope that it will be useful,
+# GSEGI is free software: you can redistribute it and/or modify
+# under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# GSEGI is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-# 
+# You should have received a copy of the GNU General Public License
+# along with GSEGI.  If not, see <http://www.gnu.org/licenses/>.
+#
 ############################################################################
 
 #import signal
